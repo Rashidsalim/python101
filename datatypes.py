c706af4eb736e18b89973a3e5010a021c6d01a34
Rashid Salim <rashid@DESKTOP-B185NVM.localdomain>
2022-10-26 20:09:40 +0300

    Add Datatypes

---
+++ datatypes.py
@@ -0,0 +1,21 @@
+# Datatypes in Python
+
+## Primitve Datatypes
+x = 6 
+y = 2.5
+
+#Python allows variable declaration
+#x,y = 5, 2.5
+
+a = b = c = 3
+
+my_String = ("Hello")
+is_true = True #Boolean Data
+is_false = False # Boolean
+my_string = [1,2,3] # list data type
+none_type = None
+
+print(type(x))
+print(type(my_string))
+print(type(is_true))
+print(type(none_type))
